261bf13bd67451c111cbff659b764810c32e3195
JudahRininger <judahrininger@gmail.com>
2022-01-29 16:53:45 -0600

    added Greet and Arithmetic portions
    I added the Greet and Arithmetic portions, but I don't exactly know what you want me to do for the Static Files portion.

--- app.js
+++ app.js
@@ -8,26 +8,34 @@ app.get('/', (req, res)=> {
     res.send("Hey, it works!");
 });
 
-// Query String Parameters
-// /pokemon?name=pikachu
-app.get('/pokemon', (req, res)=> {
+app.get('/greet', (req, res)=>{
+    let name = req.query.name;
+    let year = parseInt(req.query.year);
+    res.send(`<h1>Hello ${name}! \n You are ${2021-year} or ${2022-year} years old. <h1>`);
+})
 
-    let name = req.query.name.toUpperCase();
-    res.send(`<h1> ${name} </h1>`);
+app.get('/math', (req, res)=>{
+    res.send(`<h1> Please type "/" then a number, then times, plus, minus, or over, then another "/" and number ex: /5/plus/8 <h1>`)
+})
 
-});
-
-// Route Parameters
-// /pokemon/pikachu
-app.get('/timesten/:id', (req, res)=> {
-
-    let id = parseInt(req.params.id);
-    console.log(id);
-
-    res.send(`<h1> ${id * 10} </h1>`);
-
-});
-
+app.get('/math/:num1/:string/:num2', (req, res)=>{
+    let num1 = parseInt(req.params.num1);
+    let num2 = parseInt(req.params.num2);
+    let string = req.params.string;
+    if (string == "times") {
+        res.send(`<h1> ${num1 * num2} <h1>`)
+    };
+    if (string == "plus") {
+        res.send(`<h1> ${num1 + num2} <h1>`)
+    };
+    if (string == "minus") {
+        res.send(`<h1> ${num1 - num2} <h1>`)
+    };
+    if (string == "over") {
+        res.send(`<h1> ${num1 / num2} <h1>`)
+    };
+    res.send(`<h1> please say: times, plus, minus, or over or I won't know what to do. <h1>`)
+})
 
 app.listen(port, ()=> {
     console.log(`Listening on http://${hostname}:${port} ...`);
